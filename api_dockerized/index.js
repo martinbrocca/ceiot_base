aa6b9c545a52916eefd5b2429ddb93e1022eeb2f
Martin Brocca <103390172+martinbrocca@users.noreply.github.com>
2022-06-12 09:25:54 -0500

    added docker files

---
+++ api_dockerized/index.js
@@ -0,0 +1,157 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const {MongoMemoryServer} = require("mongodb-memory-server");
+const {MongoClient} = require("mongodb");
+const PgMem = require("pg-mem");
+
+const db = PgMem.newDb();
+
+let database = null;
+const collectionName = "measurements";
+
+async function startDatabase() {
+    const mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();	
+    const connection = await MongoClient.connect(uri, {useNewUrlParser: true});
+    database = connection.db();
+}
+
+async function getDatabase() {
+    if (!database) await startDatabase();
+    return database;
+}
+
+async function insertMeasurement(message) {
+    const {insertedId} = await database.collection(collectionName).insertOne(message);
+    return insertedId;
+}
+
+async function getMeasurements() {
+    return await database.collection(collectionName).find({}).toArray();	
+}
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static('spa'));
+app.use('/js', express.static('spa'));
+
+const PORT = 8080;
+
+app.post('/measurement', function (req, res) {
+-       console.log("device id    : " + req.body.id + " key         : " + req.body.key + " temperature : " + req.body.t + " humidity    : " + req.body.h);	
+    const {insertedId} = insertMeasurement({id:req.body.id, t:req.body.t, h:req.body.h});
+	res.send("received measurement into " +  insertedId);
+});
+
+
+app.post('/measurement2', function (req, res) {
+-       console.log("device id    : " + req.body.id + " key         : " + req.body.key + " temperature : " + req.body.t + " humidity    : " + req.body.h + " timestamp    : " + req.body.ts );	
+    const {insertedId} = insertMeasurement({id:req.body.id, t:req.body.t, h:req.body.h, ts:req.body.ts});
+	res.send("received measurement into " +  insertedId);
+});
+
+app.post('/device', function (req, res) {
+	console.log("device id    : " + req.body.id + " name        : " + req.body.n + " key         : " + req.body.k );
+
+    db.public.none("INSERT INTO devices VALUES ('"+req.body.id+ "', '"+req.body.n+"', '"+req.body.k+"')");
+	res.send("received new device");
+});
+
+
+app.get('/web/device', function (req, res) {
+	var devices = db.public.many("SELECT * FROM devices").map( function(device) {
+		console.log(device);
+		return '<tr><td><a href=/web/device/'+ device.device_id +'>' + device.device_id + "</a>" +
+			       "</td><td>"+ device.name+"</td><td>"+ device.key+"</td></tr>";
+	   }
+	);
+	res.send("<html>"+
+		     "<head><title>Sensores</title></head>" +
+		     "<body>" +
+		        "<table border=\"1\">" +
+		           "<tr><th>id</th><th>name</th><th>key</th></tr>" +
+		           devices +
+		        "</table>" +
+		     "</body>" +
+		"</html>");
+});
+
+/*
+ * Canibalized from
+ *    https://www.npmjs.com/package/sprightly
+ *    https://github.com/obadakhalili/Sprightly/blob/main/index.js
+ */
+function render(template, vars) {
+   const regexp = /<<(.*?)>>|\{\{(.*?)\}\}/;
+   return template.split('\n').map( function(line) {
+       for (let match = line.match(regexp), result; match;) {
+	   if (match[0][0] === '<') {
+		   console.log("match <");
+	   } else {
+	      result = vars[match[2].trim()];
+
+	   }
+           line = line.replace(match[0], result ? result : '');
+	   match = line.match(regexp);
+       }	       
+       return line;
+   }).join('\n');	
+}
+
+app.get('/web/device/:id', function (req,res) {
+    var template = "<html>"+
+                     "<head><title>Sensor {{name}}</title></head>" +
+                     "<body>" +
+		        "<h1>{{ name }}</h1>"+
+		        "id  : {{ id }}<br/>" +
+		        "Key : {{ key }}" +
+                     "</body>" +
+                "</html>";
+
+
+    var device = db.public.many("SELECT * FROM devices WHERE device_id = '"+req.params.id+"'");
+    console.log(device);
+    res.send(render(template,{id:device[0].device_id, key: device[0].key, name:device[0].name}));
+});	
+
+
+app.get('/term/device/:id', function (req, res) {
+    var red = "\33[31m";
+    var green = "\33[32m";
+    var blue = "\33[33m";
+    var reset = "\33[0m";
+    var template = "Device name " + red   + "   {{name}}" + reset + "\n" +
+		   "       id   " + green + "       {{ id }} " + reset +"\n" +
+	           "       key  " + blue  + "  {{ key }}" + reset +"\n";
+    var device = db.public.many("SELECT * FROM devices WHERE device_id = '"+req.params.id+"'");
+    console.log(device);
+    res.send(render(template,{id:device[0].device_id, key: device[0].key, name:device[0].name}));
+});
+
+app.get('/measurement', async (req,res) => {
+    res.send(await getMeasurements());
+});
+
+app.get('/device', function(req,res) {
+    res.send( db.public.many("SELECT * FROM devices") );
+});
+
+startDatabase().then(async() => {
+    await insertMeasurement({id:'1', t:'18', h:'78'});
+    await insertMeasurement({id:'1', t:'19', h:'77'});
+    await insertMeasurement({id:'2', t:'17', h:'77'});
+    console.log("mongo measurement database Up");
+
+    db.public.none("CREATE TABLE devices (device_id VARCHAR, name VARCHAR, key VARCHAR)");
+    db.public.none("INSERT INTO devices VALUES ('14', 'ESP32', '123456')");
+    db.public.none("INSERT INTO devices VALUES ('23', 'ESP32c3', '234567')");
+    db.public.none("INSERT INTO devices VALUES ('31', 'ESP32s2', '345678')");
+    db.public.none("INSERT INTO devices VALUES ('61', 'ESP8266', '456789')");
+    console.log("sql device database up");
+
+    app.listen(PORT, () => {
+        console.log(`Listening at ${PORT}`);
+    });
+});
